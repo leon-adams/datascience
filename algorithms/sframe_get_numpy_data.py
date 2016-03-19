a2347f1252fe379e2909c6100d4f768f32cd0d75
lradams <leon.r.adams@gmail.com>
2016-03-19 16:11:57 -0400

    Added important_words json file. Added linear-classifier and linerar-classifier-regularization notebooks.

---
+++ algorithms/sframe_get_numpy_data.py
@@ -0,0 +1,8 @@
+def get_numpy_data(data_sframe, features, label):
+    data_sframe['intercept'] = 1
+    features = ['intercept'] + features
+    features_sframe = data_sframe[features]
+    feature_matrix = features_sframe.to_numpy()
+    label_sarray = data_sframe[label]
+    label_array = label_sarray.to_numpy()
+    return(feature_matrix, label_array)
